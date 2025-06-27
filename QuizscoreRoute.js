1234433d23f1192f0a807c10d895bd3d0d6252f9
soham-06 <137515824+soham-06@users.noreply.github.com>
2025-06-27 18:42:52 +0530

    Delete QuizscoreRoute.js

--- QuizscoreRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { submitQuizScore } from "../controllers/QuizscoreController.js";
-
-const router = express.Router();
-
-router.post("/submit", submitQuizScore);
-
-export default router;
